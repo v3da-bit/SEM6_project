e9eb6f153fd6321ba294f1b60eaf5269335f65f8
v3da-bit <vedantkhamar975@gmail.com>
2022-12-19 22:55:25 +0530

    ved_commit

--- OnlineMovieTicketBS-PHP/config.php
+++ OnlineMovieTicketBS-PHP/config.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "127.0.0.1";
+    $host = "localhost";
     $user = "root";                     
     $pass = "";                                  
     $db = "movietheatredb";
     $port = 3306;
-     $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+     $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysqli_connect_error());
 ?>
